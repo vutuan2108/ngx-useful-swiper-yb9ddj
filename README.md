c76c44245d974211bf331434c60b599cbd7bea8f
vutuan2108 <vuanhtuan.humg@gmail.com>
2020-09-14 14:07:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-useful-swiper-peizgg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-useful-swiper-peizgg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SwiperOptions } from 'swiper';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+    config: SwiperOptions = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30
+  };
+}
